bce90fa6836b60356fc6ef586f8f3912b55edcd4
petervogric <petervogric@gmail.com>
2025-07-28 07:13:56 +0000

    Register and Login works

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 android {
     namespace = "com.example.myapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion // Comment or remove this line
+    ndkVersion = "27.0.12077973" // Add the required NDK version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +28,8 @@ android {
         applicationId = "com.example.myapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion // Comment or remove this line
+        minSdk = 23 // Set minSdkVersion to 23 or higher
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
